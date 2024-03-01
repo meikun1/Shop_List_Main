3e43ab91375ab4a974dfc1e1e72be3e215d2ff6c
meikun1 <150388356+meikun1@users.noreply.github.com>
2024-03-01 18:33:56 +0300

    Register
    Signed-off-by: meikun1 <150388356+meikun1@users.noreply.github.com>

---
+++ app/src/main/java/com/example/shop/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoplist.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
