10a93e3500a5aeea8e5939378794f83fb45a5765
meikun1 <150388356+meikun1@users.noreply.github.com>
2024-03-01 18:34:09 +0300

    Login
    Signed-off-by: meikun1 <150388356+meikun1@users.noreply.github.com>

---
+++ app/src/main/java/com/example/shop/AfterLoginAcrivity.kt
@@ -0,0 +1,12 @@
+package com.example.shop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoplist.R
+
+class AfterLoginAcrivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_login_acrivity)
+    }
+}
